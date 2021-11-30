10358cbe4444eff27366c1511297f51a7f222767
Ryan Baumgardner <rbaumgardner@planview.com>
2021-11-30 18:03:50 -0500

    Add more stuff for lsp

--- nvim/.config/nvim/lua/rbaumgardner/lsp.lua
+++ nvim/.config/nvim/lua/rbaumgardner/lsp.lua
@@ -90,6 +90,76 @@ dlsconfig.setup {
   }
 }
 
+
+-- credit to mhartington: https://github.com/mhartington/dotfiles/blob/main/config/nvim/lua/mh/lsp/init.lua
+local vs_code_extracted = {
+  html = "vscode-html-language-server",
+  cssls = "vscode-css-language-server",
+  vimls = "vim-language-server"
+}
+
+for ls, cmd in pairs(vs_code_extracted) do
+  nvim_lsp[ls].setup {
+    cmd = {cmd, "--stdio"},
+    on_attach = on_attach,
+    capabilities = capabilities
+  }
+end
+
+nvim_lsp.jsonls.setup {
+  cmd = {"vscode-json-language-server", "--stdio"},
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"json", "jsonc"},
+  settings = {
+    json = {
+      -- Schemas https://www.schemastore.org
+      schemas = {
+        {
+          fileMatch = {"package.json"},
+          url = "https://json.schemastore.org/package.json"
+        },
+        {
+          fileMatch = {"tsconfig*.json"},
+          url = "https://json.schemastore.org/tsconfig.json"
+        },
+        {
+          fileMatch = {
+            ".prettierrc",
+            ".prettierrc.json",
+            "prettier.config.json"
+          },
+          url = "https://json.schemastore.org/prettierrc.json"
+        },
+        {
+          fileMatch = {".eslintrc", ".eslintrc.json"},
+          url = "https://json.schemastore.org/eslintrc.json"
+        },
+        {
+          fileMatch = {".babelrc", ".babelrc.json", "babel.config.json"},
+          url = "https://json.schemastore.org/babelrc.json"
+        },
+        {
+          fileMatch = {"lerna.json"},
+          url = "https://json.schemastore.org/lerna.json"
+        },
+        {
+          fileMatch = {"vercel.json"},
+          url = "https://json.schemastore.org/now.json"
+        },
+        {
+          fileMatch = {
+            ".stylelintrc",
+            ".stylelintrc.json",
+            "stylelint.config.json"
+          },
+          url = "http://json.schemastore.org/stylelintrc.json"
+        }
+      }
+    }
+  }
+}
+
 local snippets_paths = function()
     local plugins = { "friendly-snippets" }
     local paths = {}
