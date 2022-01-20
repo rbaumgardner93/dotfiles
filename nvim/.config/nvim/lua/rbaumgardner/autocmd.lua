94f96488260ded4cb75311ae0af12eff9f9514d0
Ryan Baumgardner <rbaumgardner93@users.noreply.github.com>
2022-01-19 22:00:06 -0500

    Fix autocommand for PackerSync

--- nvim/.config/nvim/lua/rbaumgardner/autocmd.lua
+++ nvim/.config/nvim/lua/rbaumgardner/autocmd.lua
@@ -1,19 +1,19 @@
-vim.cmd [[
+vim.cmd([[
   augroup highlight_yank
       autocmd!
       au TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=700}
   augroup END
-]]
+]])
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
+vim.cmd([[
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost ./plugins.lua source <afile> | PackerSync
+    autocmd BufWritePost */plugins/init.lua source <afile> | PackerSync
   augroup end
-]]
+]])
 
 -- Disable folding on alpha buffer
-vim.cmd [[
+vim.cmd([[
     autocmd FileType alpha setlocal nofoldenable
-]]
+]])
