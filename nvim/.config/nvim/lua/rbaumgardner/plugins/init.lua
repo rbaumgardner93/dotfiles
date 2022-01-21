0c79426306f08b686e4fb50684c070d508b0b180
Ryan Baumgardner <rbaumgardner@planview.com>
2022-01-21 10:51:35 -0500

    Add which-key plugin and default config

--- nvim/.config/nvim/lua/rbaumgardner/plugins/init.lua
+++ nvim/.config/nvim/lua/rbaumgardner/plugins/init.lua
@@ -65,6 +65,12 @@ return packer.startup(function(use)
       require('rbaumgardner.plugins.alpha')
     end
   }
+  use {
+    'folke/which-key.nvim',
+    config = function ()
+      require('rbaumgardner.plugins.which-key')
+    end
+  }
 
   -- file Navigation
   use {
