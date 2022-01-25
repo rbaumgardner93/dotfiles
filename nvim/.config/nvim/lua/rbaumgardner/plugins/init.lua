9a5ee369cfbe5cb1679fd038ece7f1254c39b472
Ryan Baumgardner <rbaumgardner@planview.com>
2022-01-25 11:58:19 -0500

    Remove vim-sleuth

--- nvim/.config/nvim/lua/rbaumgardner/plugins/init.lua
+++ nvim/.config/nvim/lua/rbaumgardner/plugins/init.lua
@@ -160,7 +160,6 @@ return packer.startup(function(use)
 	use("tpope/vim-surround")
 	use("tpope/vim-eunuch")
 	use("tpope/vim-fugitive")
-	use("tpope/vim-sleuth")
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
