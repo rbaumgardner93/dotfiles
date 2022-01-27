233e97b8db6e27f5af17ec4c4d23fb0530889b93
Ryan Baumgardner <rbaumgardner@planview.com>
2022-01-27 08:17:20 -0500

    Update colorscheme settings

--- nvim/.config/nvim/lua/rbaumgardner/colorscheme.lua
+++ nvim/.config/nvim/lua/rbaumgardner/colorscheme.lua
@@ -2,13 +2,16 @@ local colorscheme = "gruvbox"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
-vim.g["gruvbox_contrast_dark"] = "hard"
-vim.g["gruvbox_invert_selection"] = 0
-vim.g["gruvbox_transparent_bg"] = 1
-
 if not status_ok then
 	vim.notify("colorscheme " .. colorscheme .. " not found!")
 	return
 end
 
-vim.cmd([[ highlight ColorColumn guibg=#333333 ]])
+vim.g["gruvbox_contrast_dark"] = "hard"
+vim.g["gruvbox_invert_selection"] = 0
+vim.g["gruvbox_transparent_bg"] = 1
+
+vim.cmd([[
+	highlight ColorColumn guibg=#333333
+	highlight LineNr guifg=#5eacd3
+]])
