c075fb884169671c7d34c0c4a12b929a9bcce560
Ryan Baumgardner <rbaumgardner@planview.com>
2022-03-18 15:36:45 -0400

    Update most autocmd to new nvim syntax

--- nvim/.config/nvim/lua/rbaumgardner/autocmd.lua
+++ nvim/.config/nvim/lua/rbaumgardner/autocmd.lua
@@ -1,39 +1,51 @@
+local nmap = require("rbaumgardner.utils.keymap").nmap
+
 vim.cmd([[
-  augroup _general
-    autocmd!
-    autocmd FileType qf,help,man,lspinfo,fugitive nnoremap <silent> <buffer> q :close<CR>
-    autocmd TextYankPost * silent! lua require('vim.highlight').on_yank({higroup="IncSearch", timeout=500})
-    autocmd BufWinEnter * :set formatoptions-=cro
-  augroup end
-
-  augroup _git
-    autocmd!
-    autocmd FileType gitcommit setlocal spell
-    autocmd FileType fugitive setlocal nofoldenable " Disable folding on fugitive buffer
-  augroup end
-
-  augroup _markdown
-    autocmd!
-    autocmd FileType markdown setlocal spell
-  augroup end
-
-  augroup _alpha
-    autocmd!
-    autocmd FileType alpha setlocal nofoldenable " Disable folding on alpha buffer
-  augroup end
-
-  augroup _harpoon
-    autocmd!
-	autocmd FileType harpoon nnoremap <buffer> <C-c> <cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>
-  augroup end
-
-  augroup _treesitter
-    autocmd!
-    autocmd FileType tsplayground setlocal nofoldenable
-  augroup end
-
   augroup _packer
     autocmd!
     autocmd BufWritePost */plugins/init.lua source <afile> | PackerSync
   augroup end
 ]])
+
+local general_group = vim.api.nvim_create_augroup("_general", { clear = true })
+local spell_group = vim.api.nvim_create_augroup("_spell", { clear = true })
+local fold_group = vim.api.nvim_create_augroup("_folds", { clear = true })
+local harpoon_group = vim.api.nvim_create_augroup("_harpoon", { clear = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	command = "silent! lua vim.highlight.on_yank( { higroup = 'IncSearch', timeout = 500 })",
+	group = general_group,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "qf", "help", "man", "lspinfo", "fugitive" },
+	callback = function()
+		nmap({ "q", "<cmd>close<CR>" })
+	end,
+	group = general_group,
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	command = "set formatoptions-=cro",
+	group = general_group,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "gitcommit", "markdown" },
+	command = "setlocal spell",
+	group = spell_group,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "fugitive", "alpha", "tsplayground" },
+	command = "setlocal nofoldenable",
+	group = fold_group,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "harpoon",
+	callback = function()
+		nmap({ "<C-c>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>" })
+	end,
+	group = harpoon_group,
+})
