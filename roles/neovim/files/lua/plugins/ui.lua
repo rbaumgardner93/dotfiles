64b69a14e244f6602d748f056768f65728268e99
Ryan Baumgardner <rbaumgardner@planview.com>
2023-06-06 16:38:25 -0400

    refactor(neovim): move setting colorscheme to plugin ui file

--- roles/neovim/files/lua/plugins/ui.lua
+++ roles/neovim/files/lua/plugins/ui.lua
@@ -1,5 +1,25 @@
 return {
-	"folke/tokyonight.nvim",
+	{
+		"folke/tokyonight.nvim",
+		config = function()
+			require("tokyonight").setup({
+				style = "night",
+				transparent = true,
+				styles = {
+					functions = "italic",
+					-- Background styles. Can be "dark", "transparent", or "normal"
+					sidebars = "transparent",
+					floats = "transparent",
+				},
+				sidebars = { "qf", "vista_kind", "terminal", "packer" },
+			})
+
+			vim.cmd([[
+				colorscheme tokyonight
+				highlight ColorColumn guibg=#292e42
+			]])
+		end,
+	},
 	{
 		"nvim-lualine/lualine.nvim",
 		event = "VeryLazy",
