593de449ca4077c1985bf99b7f30494b99174cba
Ryan Baumgardner <rbaumgardner@planview.com>
2023-08-18 10:47:05 -0400

    refactor(neovim): update keymap set method

--- roles/neovim/files/lua/keymaps.lua
+++ roles/neovim/files/lua/keymaps.lua
@@ -1,235 +1,190 @@
-vim.api.nvim_set_keymap("n", "]h", "<cmd>Gitsigns next_hunk<CR>", { noremap = true, desc = "Gitsigns: next hunk" })
+vim.keymap.set("n", "]h", "<cmd>Gitsigns next_hunk<CR>", { desc = "Gitsigns: next hunk" })
 
-vim.api.nvim_set_keymap("n", "[h", "<cmd>Gitsigns prev_hunk<CR>", { noremap = true, desc = "Gitsigns: prev hunk" })
+vim.keymap.set("n", "[h", "<cmd>Gitsigns prev_hunk<CR>", { desc = "Gitsigns: prev hunk" })
 
-vim.api.nvim_set_keymap("n", "gx", ":!open <C-r><C-a><CR>", { noremap = true, desc = "Open url under cursor" })
+vim.keymap.set("n", "gx", ":!open <C-r><C-a><CR>", { desc = "Open url under cursor" })
 
 -- [[
 -- from ThePrimeagen (https://www.youtube.com/watch?v=hSHATqh8svM)
-vim.api.nvim_set_keymap("n", "J", "mzJ`z", { noremap = true, desc = "Move line above and keep centered" })
-vim.api.nvim_set_keymap("n", "n", "nzzzv", { noremap = true, desc = "Keep centered when navigating forward" })
-vim.api.nvim_set_keymap("n", "N", "Nzzzv", { noremap = true, desc = "Keep centered when navigating backward" })
-vim.api.nvim_set_keymap("n", "Y", "y$", { noremap = true, desc = "Yank to end of line" })
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Move line above and keep centered" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Keep centered when navigating forward" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Keep centered when navigating backward" })
+vim.keymap.set("n", "Y", "y$", { desc = "Yank to end of line" })
 -- ]]
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<C-e>",
 	"<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>",
-	{ noremap = true, desc = "Harpoon: toggle quick menu" }
+	{ desc = "Harpoon: toggle quick menu" }
 )
-vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true, desc = "Keep it centered when moving down a file" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Keep it centered when moving down a file" })
 
-vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true, desc = "Keep it centered when moving up a file" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Keep it centered when moving up a file" })
 
-vim.api.nvim_set_keymap("n", "<leader><CR>", ":luafile %<CR>", { noremap = true, desc = "Fast sourcing" })
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>a",
-	"<cmd>lua require('harpoon.mark').add_file()<CR>",
-	{ noremap = true, desc = "Harpoon: add file" }
-)
-vim.api.nvim_set_keymap(
+vim.keymap.set("n", "<leader><CR>", ":luafile %<CR>", { desc = "Fast sourcing" })
+vim.keymap.set("n", "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<CR>", { desc = "Harpoon: add file" })
+vim.keymap.set(
 	"n",
 	"<leader>fb",
 	"<cmd>lua require('utils.telescope').file_browser()<cr>",
-	{ noremap = true, desc = "Telescope: File Browser" }
+	{ desc = "Telescope: [F]ile [B]rowser" }
 )
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>fc",
 	"<cmd>lua require('telescope.builtin').buffers()<cr>",
-	{ noremap = true, desc = "Telescope: Find open buffers" }
+	{ desc = "Telescope: Find open buffers" }
 )
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>ff",
 	"<cmd>lua require('utils.telescope').find_files()<cr>",
-	{ noremap = true, desc = "Telescope: Find Files" }
+	{ desc = "Telescope: [F]ind [F]iles" }
 )
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>fg",
 	"<cmd>lua require('utils.telescope').live_grep()<cr>",
-	{ noremap = true, desc = "Telescope: Grep Word" }
+	{ desc = "Telescope: Grep Word" }
 )
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>fk",
 	"<cmd>lua require('telescope.builtin').keymaps()<CR>",
-	{ noremap = true, desc = "Telescope: Find Keymaps" }
+	{ desc = "Telescope: [F]ind [K]eymaps" }
 )
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>fh",
 	"<cmd>lua require('telescope.builtin').help_tags( { show_version = true } )<cr>",
-	{ noremap = true, desc = "Telescope: Find Help" }
+	{ desc = "Telescope: [F]ind [H]elp" }
 )
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>fn",
 	"<cmd>lua require('utils.telescope').edit_neovim()<cr>",
-	{ noremap = true, desc = "Telescope: Find neovim" }
+	{ desc = "Telescope: [F]ind [n]eovim" }
 )
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>fr",
 	"<cmd>lua require('telescope.builtin').resume{}<cr>",
-	{ noremap = true, desc = "Telescope: Resume previous search" }
+	{ desc = "Telescope: Resume previous search" }
 )
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>fs",
 	"<cmd>lua require('telescope.builtin').grep_string()<cr>",
-	{ noremap = true, desc = "Telescope: Find string under cursor" }
+	{ desc = "Telescope: [F]ind [s]tring under cursor" }
 )
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>ft",
 	"<cmd>lua require('utils.telescope').curbuf()<cr>",
-	{ noremap = true, desc = "Telescope: Find in current buffer" }
+	{ desc = "Telescope: [F]ind [t]ext in current buffer" }
 )
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>fw",
 	"<cmd>lua require('utils.telescope').grep_string()<cr>",
-	{ noremap = true, desc = "Telescope: Find word" }
+	{ desc = "Telescope: [F]ind [w]ord" }
 )
 
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>a",
-	"<cmd>lua require('harpoon.mark').add_file()<CR>",
-	{ noremap = true, desc = "Harpoon: Add file" }
-)
+vim.keymap.set("n", "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<CR>", { desc = "Harpoon: Add file" })
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>ga",
 	"<cmd>lua require('gitsigns').stage_buffer()<cr>",
-	{ noremap = true, desc = "Gitsigns: stage buffer" }
+	{ desc = "Gitsigns: stage buffer" }
 )
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>gb",
 	"<cmd>lua require('gitsigns').toggle_current_line_blame()<CR>",
-	{ noremap = true, desc = "Gitsigns: toggle current line blame" }
+	{ desc = "Gitsigns: toggle current line blame" }
 )
 
-vim.api.nvim_set_keymap("n", "<leader>gB", "<cmd>Git blame<CR>", { noremap = true, desc = "Fugitive: Git blame" })
+vim.keymap.set("n", "<leader>gB", "<cmd>Git blame<CR>", { desc = "Fugitive: Git blame" })
 
-vim.api.nvim_set_keymap("n", "<leader>gd", "<cmd>Git diff<CR>", { noremap = true, desc = "Fugitive: Git diff" })
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>gh",
-	"<cmd>diffget //2<CR>",
-	{ noremap = true, desc = "Fugitive: Diff get left hand side" }
-)
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>gl",
-	"<cmd>diffget //3<CR>",
-	{ noremap = true, desc = "Fugitive: Diff get right hand side" }
-)
+vim.keymap.set("n", "<leader>gd", "<cmd>Git diff<CR>", { desc = "Fugitive: Git diff" })
+vim.keymap.set("n", "<leader>gh", "<cmd>diffget //2<CR>", { desc = "Fugitive: Diff get left hand side" })
+vim.keymap.set("n", "<leader>gl", "<cmd>diffget //3<CR>", { desc = "Fugitive: Diff get right hand side" })
 
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>gp",
-	"<cmd>lua require('gitsigns).prev_hunk()<CR>",
-	{ noremap = true, desc = "Gitsigns: Preview hunk" }
-)
+vim.keymap.set("n", "<leader>gp", "<cmd>lua require('gitsigns).prev_hunk()<CR>", { desc = "Gitsigns: Preview hunk" })
 
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>gr",
-	"<cmd>Gitsigns reset_hunk<CR>",
-	{ noremap = true, desc = "Gitsigns: Reset hunk" }
-)
+vim.keymap.set("n", "<leader>gr", "<cmd>Gitsigns reset_hunk<CR>", { desc = "Gitsigns: Reset hunk" })
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>gR",
 	"<cmd>lua require('gitsigns').reset_buffer()<CR>",
-	{ noremap = true, desc = "Gitsigns: Reset buffer" }
+	{ desc = "Gitsigns: Reset buffer" }
 )
 
-vim.api.nvim_set_keymap("n", "<leader>gs", "<cmd>G<CR>", { noremap = true, desc = "Fugitive: Git status" })
+vim.keymap.set("n", "<leader>gs", "<cmd>G<CR>", { desc = "Fugitive: Git status" })
 
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>gS",
-	"<cmd>Gitsigns stage_hunk<CR>",
-	{ noremap = true, desc = "Gitsigns: stage hunk" }
-)
+vim.keymap.set("n", "<leader>gS", "<cmd>Gitsigns stage_hunk<CR>", { desc = "Gitsigns: stage hunk" })
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>gu",
 	"<cmd>lua require('gitsigns').undo_stage_hunk()<CR>",
-	{ noremap = true, desc = "Gitsigns: undo stage hunk" }
+	{ desc = "Gitsigns: undo stage hunk" }
 )
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>h",
 	"<cmd>lua require('harpoon.ui').nav_file(1)<CR>",
-	{ noremap = true, desc = "Harpoon: navigate to first file" }
+	{ desc = "Harpoon: navigate to first file" }
 )
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>j",
 	"<cmd>lua require('harpoon.ui').nav_file(2)<CR>",
-	{ noremap = true, desc = "Harpoon: navigate to second file" }
+	{ desc = "Harpoon: navigate to second file" }
 )
-vim.api.nvim_set_keymap("n", "<leader>J", ":m .+1<CR>==", { noremap = true, desc = "Move line down" })
+vim.keymap.set("n", "<leader>J", ":m .+1<CR>==", { desc = "Move line down" })
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>k",
 	"<cmd>lua require('harpoon.ui').nav_file(3)<CR>",
-	{ noremap = true, desc = "Harpoon: navigate to third file" }
+	{ desc = "Harpoon: navigate to third file" }
 )
 
-vim.api.nvim_set_keymap("n", "<leader>K", ":m .-2<CR>==", { noremap = true, desc = "Move line up" })
+vim.keymap.set("n", "<leader>K", ":m .-2<CR>==", { desc = "Move line up" })
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
 	"n",
 	"<leader>l",
 	"<cmd>lua require('harpoon.ui').nav_file(4)<CR>",
-	{ noremap = true, desc = "Harpoon: navigate to fourth file" }
+	{ desc = "Harpoon: navigate to fourth file" }
 )
 
-vim.api.nvim_set_keymap("n", "<leader>md", "<cmd>Glow<CR>", { noremap = true, desc = "Glow: Preview markdown" })
-vim.api.nvim_set_keymap("n", "<leader>ut", "<cmd>UndotreeToggle<CR>", { noremap = true, desc = "UndoTree: Toggle" })
+vim.keymap.set("n", "<leader>md", "<cmd>Glow<CR>", { desc = "Glow: Preview markdown" })
+vim.keymap.set("n", "<leader>ut", "<cmd>UndotreeToggle<CR>", { desc = "UndoTree: Toggle" })
 
-vim.api.nvim_set_keymap("n", "<leader>w", ":w!<CR>", { noremap = true, desc = "Save file" })
+vim.keymap.set("n", "<leader>w", ":w!<CR>", { desc = "Save file" })
 
-vim.api.nvim_set_keymap("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, desc = "Move selected line down" })
-vim.api.nvim_set_keymap("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, desc = "Move selected line up" })
-vim.api.nvim_set_keymap("v", "y", "y']", { noremap = true, desc = "Yank to register" })
-vim.api.nvim_set_keymap("v", ">", ">gv", { noremap = true, desc = "Indent" })
-vim.api.nvim_set_keymap("v", "<", "<gv", { noremap = true, desc = "Dedent" })
-vim.api.nvim_set_keymap(
-	"v",
-	"<leader>gr",
-	"<cmd>Gitsigns reset_hunk<CR>",
-	{ noremap = true, desc = "Gitsigns: reset hunk" }
-)
-vim.api.nvim_set_keymap(
-	"v",
-	"<leader>gs",
-	"<cmd>Gitsigns stage_hunk<CR>",
-	{ noremap = true, desc = "Gitsigns: stage hunk" }
-)
-vim.api.nvim_set_keymap("v", "<leader>p", "_dP", { noremap = true, desc = "Paste below" })
-vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true, desc = "Yank to system clipboard" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected line down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected line up" })
+vim.keymap.set("v", "y", "y']", { desc = "Yank to register" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent" })
+vim.keymap.set("v", "<", "<gv", { desc = "Dedent" })
+vim.keymap.set("v", "<leader>gr", "<cmd>Gitsigns reset_hunk<CR>", { desc = "Gitsigns: reset hunk" })
+vim.keymap.set("v", "<leader>gs", "<cmd>Gitsigns stage_hunk<CR>", { desc = "Gitsigns: stage hunk" })
+vim.keymap.set("v", "<leader>p", "_dP", { desc = "Paste below" })
+vim.keymap.set("v", "<leader>y", '"+y', { desc = "Yank to system clipboard" })
 
-vim.api.nvim_set_keymap("t", "<ESC>", "<C-\\><C-n>", { noremap = true, desc = "Close terminal" })
+vim.keymap.set("t", "<ESC>", "<C-\\><C-n>", { desc = "Close terminal" })
