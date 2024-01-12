8a6bc07bcdf583621b4c34f84e4075b85a5f9253
Ryan Baumgardner <rbaumgardner@planview.com>
2024-01-12 08:51:33 -0500

    style(neovim): format git file

--- roles/neovim/files/lua/plugins/git.lua
+++ roles/neovim/files/lua/plugins/git.lua
@@ -1,37 +1,39 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	event = "BufReadPre",
-	config = function()
-		local gitsigns = require("gitsigns")
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "BufReadPre",
+		config = function()
+			local gitsigns = require("gitsigns")
 
-		gitsigns.setup({
-			signs = {
-				add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-				change = {
-					hl = "GitSignsChange",
-					text = "▎",
-					numhl = "GitSignsChangeNr",
-					linehl = "GitSignsChangeLn",
+			gitsigns.setup({
+				signs = {
+					add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+					change = {
+						hl = "GitSignsChange",
+						text = "▎",
+						numhl = "GitSignsChangeNr",
+						linehl = "GitSignsChangeLn",
+					},
+					delete = {
+						hl = "GitSignsDelete",
+						text = "▎",
+						numhl = "GitSignsDeleteNr",
+						linehl = "GitSignsDeleteLn",
+					},
+					topdelete = {
+						hl = "GitSignsDelete",
+						text = "▎",
+						numhl = "GitSignsDeleteNr",
+						linehl = "GitSignsDeleteLn",
+					},
+					changedelete = {
+						hl = "GitSignsChange",
+						text = "▎",
+						numhl = "GitSignsChangeNr",
+						linehl = "GitSignsChangeLn",
+					},
 				},
-				delete = {
-					hl = "GitSignsDelete",
-					text = "▎",
-					numhl = "GitSignsDeleteNr",
-					linehl = "GitSignsDeleteLn",
-				},
-				topdelete = {
-					hl = "GitSignsDelete",
-					text = "▎",
-					numhl = "GitSignsDeleteNr",
-					linehl = "GitSignsDeleteLn",
-				},
-				changedelete = {
-					hl = "GitSignsChange",
-					text = "▎",
-					numhl = "GitSignsChangeNr",
-					linehl = "GitSignsChangeLn",
-				},
-			},
-		})
-	end,
+			})
+		end,
+	},
 }
