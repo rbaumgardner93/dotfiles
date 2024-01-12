8c33079dd72f2e63dc50a27e4c53f8f8299f660c
Ryan Baumgardner <rbaumgardner@planview.com>
2024-01-12 08:54:32 -0500

    feat(neovim): set default node version for neovim

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -1,3 +1,17 @@
+-- [[ https://jaketrent.com/post/set-node-version-nvim/
+
+local is_mac = vim.fn.has("macunix") == 1
+local home_dir = "/home/rbaumgardner/"
+if is_mac then
+	home_dir = "/Users/rbaumgardner/"
+end
+
+local node_bin = home_dir .. ".fnm/node-versions/v18.15.0/installation/bin"
+vim.g.node_host_prog = node_bin .. "/node"
+vim.env.PATH = node_bin .. ":" .. vim.env.PATH
+
+-- ]]
+
 require("options")
 require("package_manager")
 require("keymaps")
